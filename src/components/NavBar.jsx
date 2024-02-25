180cff0163e123a1a93a133db221616389c54ca0
Veikka <veikka.nevala@outlook.com>
2024-02-25 14:33:06 +0200

    navbar functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,41 @@
+import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+
 function NavBar () {
+    const [navActive, setNavActive] = useState(false)
+    const navigate = useNavigate()
+    const handleClick = () => {
+        setNavActive(!navActive)
+    }
+
+    const handleNavigation = (event) => {
+        switch (event.target.innerText) {
+            case "opiskelu":
+                navigate("/opiskelu/")
+                break
+            case "johdanto":
+                navigate("/")
+                break
+            case "asuminen":
+                navigate("/asuminen/")
+                break
+            case "matkustaminen":
+                navigate("/matkustaminen/")
+                break
+        }
+    }
+
     return (
         <div className="navbar">
-            <p>navbar</p>
+            <p id="navigator" onClick={handleClick}>nav</p>
+            {navActive &&
+            <>
+                <p onClick={handleNavigation}>johdanto</p>
+                <p onClick={handleNavigation}>opiskelu</p>
+                <p onClick={handleNavigation}>asuminen</p>
+                <p onClick={handleNavigation}>matkustaminen</p>
+            </> 
+            }
         </div>
     )
 }
