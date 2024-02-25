1fb4841fb36cf04d32a0dc3e9e3134feae59bb79
Veikka <veikka.nevala@outlook.com>
2024-02-25 13:34:40 +0200

    johdanto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-
+import NavBar from "./components/NavBar"
+import Footer from "./components/Footer"
 
 function App() {
-  
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <h1>Opiskelijaelämää Joensuusssa</h1>
+        <div className="textbox">
+          <h2>otsikko</h2>
+          <p>tekstiä</p>
+          <p>tekstiä</p>
+          <p>tekstiä</p>
+          <p>tekstiä</p>
+          <p>tekstiä</p>
+          <p>tekstiä</p>
+          <p>tekstiä</p>
+          <p>tekstiä</p>
+          <p>tekstiä</p>
+        </div>
+        <Footer />
+      </div>
+      
+    </>
+    
+  )
 }
 
 export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+function NavBar () {
+    return (
+        <div className="navbar">
+            <p>navbar</p>
+        </div>
+    )
+}
+
+export default NavBar
