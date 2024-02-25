cf5b065be10520820b2b4f4898d464cf1cc22c39
Veikka <veikka.nevala@outlook.com>
2024-02-25 13:44:22 +0200

    router setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Opiskelu from './pages/opiskelu.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/opiskelu/",
+    element: <Opiskelu />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/opiskelu.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar"
+
+function Opiskelu() {
+    return (
+        <>
+            <NavBar />
+            <p>opiskelu</p>
+        </>
+    )
+}
+
+export default Opiskelu
