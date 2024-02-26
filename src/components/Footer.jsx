7a4d031ddc137f48c586421e5b069f9bec5d86a8
Veikka <veikka.nevala@outlook.com>
2024-02-27 00:28:17 +0200

    footer improved

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,20 @@
 function Footer() {
     return (
         <div className="footer">
-            <p>footer</p>
+            <div>
+                <h5>Teksti</h5>
+                <p>Teksti luotu <a href="https://chat.openai.com/">ChatGPT:n</a> avulla</p>
+            </div>
+            <div>
+                <h5>Kuvat</h5>
+                <p>Kaikki kuvat ovat itseotettuja <br /> vuosilta 2022-2024</p>
+            </div>
+            <div>
+                <h5>Yhteystiedot</h5>
+                <p>Veikka Nevala 2024</p>
+                <p>vnevala@uef.fi</p>
+                <p>80100 Joensuu</p>
+            </div>
         </div>
     )
 }
