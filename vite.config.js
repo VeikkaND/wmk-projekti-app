d990ca5c2db0bc1c28f23d0da38d4031c80576c9
Veikka <veikka.nevala@outlook.com>
2024-02-27 14:25:38 +0200

    config changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/wmk-projekti-app/"
+  plugins: [react()]
 })
